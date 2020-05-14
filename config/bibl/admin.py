5a9461cc285651d2c874b8a54b969dedf021a6ac
MaximoSav <maximosavino10@gmail.com>
2020-05-14 20:56:40 -0300

    Biblioteca terminada

--- config/bibl/admin.py
+++ config/bibl/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from bibl.models import Autor
+from bibl.models import Libro
+from bibl.models import Ejemplar
+from bibl.models import Usuarios
 # Register your models here.
+
+admin.site.register(Autor,)
+admin.site.register(Libro,)
+admin.site.register(Ejemplar,)
+admin.site.register(Usuarios,)
--- config/bibl/models.py
+++ config/bibl/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+	nombre = models.CharField(max_length = 50)
+	codigo = models.IntegerField(primary_key=True)
+	
+	def __str__(self):
+		return ("{}:{}".format(self.nombre, self.codigo))
+
+class Libro (models.Model):
+	titulo = models.CharField(max_length = 50)
+	editorial = models.CharField(max_length = 50)
+	cant_pag = models.IntegerField()
+	codigo = models.IntegerField(primary_key=True)
+	codAutor = models.ForeignKey(Autor, on_delete=models.CASCADE, null = True)
+	
+	def __str__(self):
+		return ("{}:{}".format(self.titulo, self.codAutor))
+
+class Ejemplar (models.Model):
+	localizacion = models.CharField(max_length = 50, null = True) 
+	codigo = models.IntegerField(primary_key=True)
+	codLibro = models.ForeignKey(Libro, on_delete=models.CASCADE, null = True)
+
+	def __str__(self):
+		return ("{}:{}".format(self.localizacion, self.codLibro))
+
+class Usuarios (models.Model):
+	nombre = models.CharField(max_length = 50)
+	codigo = models.IntegerField(primary_key=True)
+	telefono = models.IntegerField()
+	direccion = models.CharField(max_length = 50, null = True)
+
+	def __str__(self):
+		return ("{}:{}".format(self.nombre, self.codigo))
+		
+
+	
+
+
+
+
+
